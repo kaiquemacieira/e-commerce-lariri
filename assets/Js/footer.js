1279ea57a891cd3186043e59efc5f39915b95118
kaique <kaique@kaiqs6669>
2022-02-26 20:53:30 -0300

    assets

---
+++ assets/Js/footer.js
@@ -0,0 +1,41 @@
+const createFooter = () => {
+    let footer = document.querySelector('footer');
+
+    footer.innerHTML = `
+    <div class="footer-content">
+            <img src="Img/light-logo.png" class="logo" alt="">
+            <div class="footer-ul-container">
+                <ul class="category">
+                    <li class="category-title">men</li>
+                    <li><a href="#" class="footer-link">camisetas</a></li>
+                    <li><a href="#" class="footer-link">acessórios</a></li>
+                    <li><a href="#" class="footer-link">croppeds</a></li>
+                </ul>
+                <ul class="category">
+                    <li class="category-title">wommen</li>
+                    <li><a href="#" class="footer-link">camisetas</a></li>
+                    <li><a href="#" class="footer-link">acessórios</a></li>
+                    <li><a href="#" class="footer-link">croppeds</a></li>
+                </ul>
+            </div>
+            
+
+        </div>
+        <p class="footer-title">Sobre a empresa</p>
+        <p class="info">Somos uma empresa bla bla bla bla bla</p>
+        <p class="info">suporte email - comunaseregy@email.com</p>
+        <p class="info">contato - (+55) 79-8899-9988</p>
+        <div class="footer-social-container">
+            <div>
+            <a href="#" class="social-link">termos de serviço</a>
+            <a href="#" class="social-link">página privada</a>
+        </div>
+        <div>
+            <a href="#" class="social-link">instagram</a>
+            <a href="#" class="social-link">whatsapp</a>
+            <a href="#" class="social-link">twitter</a>
+        </div>
+        </div>
+        <p class="footer-credit">K.</p>
+        `;
+}
---
+++ assets/Js/nav.js
@@ -0,0 +1,25 @@
+const createNav = () => {
+    let nav = document.querySelector('.navbar');
+    nav.innerHTML = `
+    <div class="nav">
+                <img src="./Img/dark-logo.png" class="brand-logo" alt="">
+        
+            <div class="nav-itens">
+            <div class="search">
+                <input type="text" class="search-box" placeholder="Pesquise seu produto">
+                <button class="search-btn">Search</button>
+            </div>
+            <a href="#"><img src="./Img/user.png" alt=""></a>
+            <a href="#"><img src="./Img/cart.png" alt=""></a>
+            </div>
+        </div>
+        <ul class="links-container">
+            <li class="link-item"><a href="#" class="link">início</a></li>
+            <li class="link-item"><a href="#" class="link">acessórios</a></li>
+            <li class="link-item"><a href="#" class="link">ecobags</a></li>
+            
+        </ul>
+    `;
+}
+
+createNav();
