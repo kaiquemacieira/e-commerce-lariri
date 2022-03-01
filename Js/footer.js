55a95444293817c2aa3ee035eb7e1213aca3683b
kaique <kaique@kaiqs6669>
2022-03-01 00:53:42 -0300

    incremento

--- Js/footer.js
+++ Js/footer.js
@@ -4,17 +4,13 @@ const createFooter = () => {
     footer.innerHTML = `
     <div class="footer-content">
         <img src="Img/logologo.png" class="logo" alt="">
-            <div class="footer-ul-container">
-                
-            </div>
-            
-
-        </div>
+           
+    </div>
         <p class="footer-title">Sobre a empresa</p>
         <p class="info">Somos uma empresa bla bla bla bla bla</p>
         <p class="info">suporte email - comunaseregy@email.com</p>
         <p class="info">contato - (+55) 79-8899-9988</p>
-        <div class="footer-social-container">
+    <div class="footer-social-container">
             <div>
             <a href="#" class="social-link">termos de serviço</a>
             <a href="#" class="social-link">página privada</a>
@@ -24,7 +20,7 @@ const createFooter = () => {
             <a href="#" class="social-link">whatsapp</a>
             <a href="#" class="social-link">twitter</a>
         </div>
-        </div>
+    </div>
         <p class="footer-credit">K.</p>
         `;
 }
